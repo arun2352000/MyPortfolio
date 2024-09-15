54e74aa5f5c4e2009727385d5c189b68bc041d7a
arun2352000 <poorvaja10@gmail.com>
2024-09-15 19:06:51 +0530

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,27 @@ import './style/home.css'
 const Home = () => {
     return (
       <div>
-        <div>
-          <h2>Hello,</h2>
-          <h1>I'm Arunachalam,</h1>
-          <h3>
-            a MERN stack web developer bringing a fresh perspective to web
-            design.
-          </h3>
-
-          <img
-            src="/src/assets/avatar-image.png"
-            alt=""
-            className="profile-image img-fluid"
-          />
+        
+        <div className='flex'>
+          <div className='w-1/2'>
+            <h2 className=''>Hello,</h2>
+            <h1>I'm Arunachalam,</h1>
+            <h3>
+              a MERN stack web developer bringing a fresh perspective to web
+              design.
+            </h3>
+            </div>
+            <img 
+              src="/src/assets/avatar-image.png"
+              alt=""
+              className=""
+            />
         </div>
-        <div>
+        
+      
+      <div className=''>
           
-        <h1>About Me</h1> 
+        <h1>About Me</h1> <br />
         <p>Hello, I'm Arunachalam, a passionate MERN stack developer with a strong background in building dynamic and responsive web applications.
            I have a solid foundation in MongoDB, Express.js, React, and Node.js, and I am dedicated to delivering high-quality solutions that meet client needs.
            I thrive on challenges and am always eager to learn and grow in the ever-evolving tech landscape.
@@ -27,6 +31,7 @@ const Home = () => {
            </p>
           
         </div>
+        
       </div>
     );
 };
