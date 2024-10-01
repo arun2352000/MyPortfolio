04095a948c252116057a39b1ea7397cd869df644
arun2352000 <poorvaja10@gmail.com>
2024-10-02 00:12:55 +0530

    contact form feal update

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,6 +4,22 @@ const ContactForm = () => {
     return (
         <div>
             <h1>contact form test page</h1>
+                <form action="">
+                    <label for="name">Name:</label>
+                    <input type="text" id="name" name="name" required/>
+                    <br/>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required/>
+                    <br/>
+                    <label for="Number">Number:</label>
+                    <input type="number" id="Number" name="Number" required/>
+                    <br />
+                    <label for="Message">Message:</label>
+                    <textarea id="message" name="message" required/>
+                    <br/>
+                    {/* <button type="submit"></button> */}
+                    <input type="submit" value="Submit"></input>
+                </form>
         </div>
     );
 };
