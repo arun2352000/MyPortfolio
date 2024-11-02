616ede1b817debc18be9d774839f2567c1df21dd
arun2352000 <poorvaja10@gmail.com>
2024-11-02 09:02:14 +0530

    space error cleared on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div id='about'className='flex flex-col md:flex-row bg-tertiary px-5'>
+        <div id='about'className='flex flex-col md:flex-row  px-5 py-10 bg-tertiary'>
             <div className='md:w-1/2 py-5'>
                 <img src="/assets/avatar-image_2.png" alt="" />
             </div>
-            <div className='md:w-1/2 flex justify-center'>
+            <div className='md:w-1/2  flex justify-center'>
                 <div className='flex flex-col justify-center'>
                 <h1 className='text-5xl font-title'>About Me</h1> <br />
                 <p className='text-2xl font-text'>Hello, I'm Arunachalam, a passionate MERN stack developer with a strong background in building dynamic and responsive web applications. <br />
